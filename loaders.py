a2939e7ba7cb838cf6a63de47290a4dba555e635
jonhsel <jonhselmo.engcomp@gmail.com>
2024-12-25 14:28:15 -0300

    atualização de arquivos

--- loaders.py
+++ loaders.py
@@ -1,3 +1,4 @@
+
 from langchain_community.document_loaders import (WebBaseLoader,
                                                   YoutubeLoader,
                                                   CSVLoader,

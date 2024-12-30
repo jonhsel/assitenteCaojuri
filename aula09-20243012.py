13b6e8be64b42fc8da0c924bc366a41e25dd598a
jonhsel <jonhselmo.engcomp@gmail.com>
2024-12-30 07:14:22 -0300

    ajuste botão largura

--- aula09-20243012.py
+++ aula09-20243012.py
@@ -149,10 +149,10 @@ def sidebar():
         st.session_state[f'api_key_{provedor}'] = api_key
 
 
-    if st.button('Iniciar o Assistente'):
+    if st.button('Iniciar o Assistente', use_container_width=True):
         carrega_modelo(provedor, modelo, api_key, tipo_arquivo, arquivo)
 
-    if st.button('Limpar o histórico de conversação'):
+    if st.button('Limpar o histórico de conversação', use_container_width=True):
         st.session_state['memoria'] = MEMORIA
 
 def main():
